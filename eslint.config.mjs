dab4d3633ca60ad1dfbcfc9b510f58d526dc21e0
Tamaroh <tamaroh@gmail.com>
2024-12-05 11:25:22 +0900

    fixed eslint-jest-flatconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+import eslintJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
+  eslintJest.configs["flat/style"],
+  eslintJest.configs["flat/recommended"],
   pluginJs.configs.recommended,
 ];
